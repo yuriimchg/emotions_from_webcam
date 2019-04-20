bace768fb83a23351757d399ddc7bea833f88d93
yuriimchg <yurgan4@gmail.com>
2019-04-20 23:16:14 +0300

    able to recognize landmarks

--- landmarks.py
+++ landmarks.py
@@ -13,19 +13,28 @@ def shape_to_np(shape, dtype="int"):
 
     return coords
 
-landmark_path = ''
+landmark_path = 'face_landmarks.dat'
 img_path = os.path.join('media', 'sample1.png')
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor(landmark_path)
 
 image = cv2.imread(img_path)
-gray_image = cv2.cvtColor(image, cv2.COLOR_BRG2GRAY)
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 rects = detector(gray_image, 1)
 
 for i, rect in enumerate(rects):
 
     shape = predictor(gray_image, rect)
-    x, y = rect.left(), rect.top()
-    w, h = rect.right() - x, rect.bottom() - y
+    shape = shape_to_np(shape)
+    x1, y1 = rect.left(), rect.top()
+    x2, y2 = rect.right(), rect.bottom()
+    cv2.rectangle(image, (x1, y1), (x2, y2), (255, 0, 255), 2)
+
+    for x, y in shape:
+        cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
+
+cv2.imshow('fuck you', image)
+if cv2.waitKey(0) & 0xFF == ord('z'):
+    cv2.destroyAllWindows()
