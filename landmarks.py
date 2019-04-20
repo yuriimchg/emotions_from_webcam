beb8d7e5909390766d78d1d1ad8979148d85fcc1
yuriimchg <yurgan4@gmail.com>
2019-04-21 01:55:01 +0300

    methods

--- landmarks.py
+++ landmarks.py
@@ -5,39 +5,41 @@ import dlib
 import cv2
 
 
-def shape_to_np(shape, dtype="int"):
-    coords = np.zeros((68, 2), dtype=dtype)
-
-    for i in range(0, 68):
-        coords[i] = (shape.part(i).x, shape.part(i).y)
-
-    return coords
-
 landmark_path = 'face_landmarks.dat'
 img_path = os.path.join('media', 'sample1.png')
-
 detector = dlib.get_frontal_face_detector()
+capture = cv2.VideoCapture(0)
 predictor = dlib.shape_predictor(landmark_path)
 
-# image = cv2.imread(img_path)
-capture = cv2.VideoCapture(0)
 
-while True:
-    ret, image = capture.read()
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+def shape_to_np(shape, dtype="int", landmarks_count=68):
+    point = np.zeros((landmarks_count, 2), dtype=dtype)
 
+    for i in range(landmarks_count):
+        point[i] = (shape.part(i).x, shape.part(i).y)
+
+    return point
+
+def get_face_landmarks(img_array, predictor):
+    gray_image = cv2.cvtColor(img_array, cv2.COLOR_BGR2GRAY)
     rects = detector(gray_image, 1)
 
     for i, rect in enumerate(rects):
 
-        shape = predictor(gray_image, rect)
-        shape = shape_to_np(shape)
+        face_detector = predictor(gray_image, rect)
+        shape = shape_to_np(face_detector)
         x1, y1 = rect.left(), rect.top()
         x2, y2 = rect.right(), rect.bottom()
-        cv2.rectangle(image, (x1, y1), (x2, y2), (255, 0, 255), 2)
 
+        cv2.rectangle(image, (x1, y1), (x2, y2), (255, 0, 255), 2)
         for x, y in shape:
-            cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
+            cv2.circle(image, (x, y), 2, (255, 255, 255), -1)
+
+
+while True:
+    ret, image = capture.read()
+    get_face_landmarks(image, predictor)
+
 
     cv2.imshow('Press "q" to close the window', image)
     if cv2.waitKey(1) & 0xFF == ord('q'):
